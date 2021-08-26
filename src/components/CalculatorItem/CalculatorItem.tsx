7b258e1c1603a2e27f5d4941108389ff871eeefd
Joshua Fredrickson <joshua@orangepineapple.com>
2021-08-26 16:59:04 -0500

    CalculatorItem: Add min/max to inputs

--- src/components/CalculatorItem/CalculatorItem.tsx
+++ src/components/CalculatorItem/CalculatorItem.tsx
@@ -26,12 +26,14 @@ const CalculatorItem: React.FC<CalculatorItemProps> = (props) => {
   };
 
   return (
-    <p className="flex flex-wrap mb-6 CalculatorItem">
+    <p className="flex flex-wrap w-full mb-6 CalculatorItem">
       <span className="w-full">{types[props.type].label}</span>
       <span className="flex items-center w-full">
         <input
           type="number"
           className="w-full p-2 mr-2 text-lg bg-gray-700 border border-gray-900 rounded CalculatorItem__input"
+          min="0"
+          max={types[props.type].units === '%' ? '100' : undefined}
         />
         <span className="CalculatorItem__unit">{types[props.type].units}</span>
       </span>
