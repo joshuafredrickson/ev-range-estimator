39ecb207d02283323f70f3fc370434253e468b77
Joshua Fredrickson <joshua@orangepineapple.com>
2021-08-31 19:07:57 -0500

    Add efficiency placeholder

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -1,30 +1,29 @@
 import React from 'react';
 
 export interface ResultProps {
-  type: 'remaining' | 'total';
+  type: 'remaining' | 'total' | 'efficiency';
   value?: number;
 }
 
 const Results: React.FC<ResultProps> = (props) => {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full p-8 text-gray-800">
-      <span className="block text-3xl whitespace-nowrap">
+    <div
+      className={`flex flex-col items-center justify-center landscape:w-full ${
+        props.type === 'efficiency'
+          ? 'bg-green-700 text-white h-auto w-full p-3'
+          : 'text-gray-800 landscape:h-full w-1/2 p-6'
+      }`}
+    >
+      <span
+        className={`block whitespace-nowrap ${
+          props.type === 'efficiency' ? 'text-xl' : 'text-3xl'
+        }`}
+      >
         {props.value ?? <>&ndash;</>} mi
+        {props.type === 'efficiency' ? '/kwh' : ''}
       </span>
-      {props.type === 'remaining' && (
-        <>
-          Est. Range
-          <br />
-          Remaining
-        </>
-      )}
-      {props.type === 'total' && (
-        <>
-          Est. Total
-          <br />
-          Range
-        </>
-      )}
+      {props.type === 'remaining' && <>Remaining</>}
+      {props.type === 'total' && <>Total Range</>}
     </div>
   );
 };
--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -4,9 +4,10 @@ import './Results.scss';
 
 const Results: React.FC = () => {
   return (
-    <div className="flex flex-row text-center bg-electric landscape:flex-col Results">
+    <div className="flex flex-row flex-wrap text-center bg-electric landscape:flex-col landscape:flex-nowrap Results">
       <Result type="remaining" value={30} />
       <Result type="total" value={124} />
+      <Result type="efficiency" value={4.8} />
     </div>
   );
 };
