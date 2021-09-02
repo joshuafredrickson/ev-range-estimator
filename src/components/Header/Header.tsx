f54d85c66fcf8ca9bffe12bb67f25e35b220a77c
Joshua Fredrickson <joshua@orangepineapple.com>
2021-09-02 14:25:23 -0500

    Add GitHub link to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './Header.scss';
 import logo from '../../assets/images/logo.svg';
+import github from '../../assets/images/github.svg';
 
 const Header: React.FC = () => {
   return (
-    <header className="p-2 Header">
+    <header className="flex items-center justify-between p-2 Header">
       <a href="/">
         <img src={logo} style={{ width: '3rem' }} alt="EV Range Estimator" />
+      </a>
+
+      <a
+        href="https://github.com/joshuafredrickson/ev-range-estimator"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src={github}
+          alt="View project on GitHub"
+          style={{ width: '1rem' }}
+        />
       </a>
     </header>
   );
